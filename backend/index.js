83718c791735d3cbd9e927e3fa04e1b521640ede
ramya1102 <ramyapanditi@gmail.com>
2023-11-20 19:15:42 +0530

    To deploy3

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,24 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const route = require("./controller/tasksRoute");
+    const express = require("express");
+    const app = express();
+    const bodyParser = require("body-parser");
+    const cors = require("cors");
+    const route = require("./controller/tasksRoute");
 
-const mongoose = require("mongoose");
-mongoose.set("strictQuery",true);
-mongoose.connect("mongodb+srv://ramyapanditi:12345@cluster0.rv0giqd.mongodb.net/");
-const db = mongoose.connection;
-db.on("open",()=> console.log("database connected"));
-db.on("error",() => console.log("error"));
+    const mongoose = require("mongoose");
+    mongoose.set("strictQuery",true);
+    mongoose.connect("mongodb+srv://ramyapanditi:12345@cluster0.rv0giqd.mongodb.net/");
+    const db = mongoose.connection;
+    db.on("open",()=> console.log("database connected"));
+    db.on("error",() => console.log("error"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors(
-    {
-        origin:"*",
-        methods:["POST","GET","PUT","DELETE"]
-    }
-));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(cors({
+        origin: "https://taskpilot-chi.vercel.app/",
+        methods: ["POST", "GET", "PUT", "DELETE"],
+      }));
+    app.use("/",route);
 
-
-app.use("/",route);
-
-app.listen(4000,()=>{
-    console.log("Running on 4000");
-})
+    app.listen(4000,()=>{
+        console.log("Running on 4000");
+    })
