aaac132f8d496e44cc79fc3cd11637ee264cf1fe
ramya1102 <ramyapanditi@gmail.com>
2023-11-20 19:50:50 +0530

    hope

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,21 @@
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    
+    app.use("/",route);
     app.use(cors({
         origin: ["https://taskpilot-chi.vercel.app","https://taskpilot-5z61.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
       }));
     app.options("*", cors());
-
-    app.use("/",route);
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "https://taskpilot-5z61.vercel.app");
+        res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Credentials", true);
+        next();
+    });
+    
 
     app.listen(4000,()=>{
         console.log("Running on 4000");
